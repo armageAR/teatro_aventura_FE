d207f4674640c9375529421eb50bcf8f97c0fe0d
Ariel Gerling <armage@gmail.com>
2025-06-09 22:31:09 -0300

    fix: nombre

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: 'Teatro Aventura FrontEnd',
+  title: 'Teatro Aventura',
   description: 'FrontEnd para la app Teatro Aventura',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://armage.tech',
