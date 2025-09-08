2b017c1fb6985996090d9034346e43e5fa5fc38a
Ariel Gerling <armage@gmail.com>
2025-09-08 15:10:32 -0300

    fix: prod api url

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,6 +1,6 @@
 import { User } from '@/lib/types/user';
 
-export const API_BASE_URL = 'http://localhost:8008/api';
+export const API_BASE_URL = 'https://teatroaventurabe.up.railway.app/api';
 
 export interface ResetPasswordData {
   password: string;
